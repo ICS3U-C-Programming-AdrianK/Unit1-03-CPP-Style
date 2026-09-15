24f6715d5a498ba5d82e276b0ffb4c762762e4d9
Adrian kouakam <adrian.kouakam-guemiebing@stu.ocsb.ca>
2026-09-15 13:36:24 +0000

    First commit helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2026 Adrian Student All rights reserved.
+// .
+// Created by: Adrian student
+// Date: sep 15th, 2026
+// This program displays a greeting with proper style .
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello adrian \n";
+    std::cout << "This class is exciting \n";
+}
